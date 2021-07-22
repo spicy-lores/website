056c822a0b3e0b61d513bb56892645f092462406
Sooraj Parameswaran <srjpn123@gmail.com>
2021-07-22 20:42:15 +0530

    Setting up coming soon page

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/app.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+function Application({ Component, pageProps }) {
+    return <Component {...pageProps} />
+  }
+  
+  export default Application
